f765f3a9d8734e4cb5eb408373eaf74c96919720
gpw <gorjux_p@epita.fr>
2014-12-06 09:52:45 +0100

    Started Entity

--- include/entity.hh
+++ include/entity.hh
@@ -3,6 +3,14 @@
 
 class Entity
 {
+  public:
+    float get_x();
+    float get_y();
+    float get_direction();
+  private:
+    float x_;
+    float y_;
+    float direction_;
 };
 
 #endif /* !ENTITY_HH */
