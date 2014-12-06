3843090f26ebd4985b97963d3544ed60015fc3b1
gpw <gorjux_p@epita.fr>
2014-12-06 09:58:35 +0100

    Entitiess basic getters: x, y & dir

---
+++ src/entity.cc
@@ -0,0 +1,16 @@
+#include "entity.hh"
+
+float Entity::get_x()
+{
+  return this->x_;
+}
+
+float Entity::get_y()
+{
+  return this->y_;
+}
+
+float Entity::get_direction()
+{
+  return this->direction_;
+}
